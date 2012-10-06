5897c1b3f3fb520aaa9e527603eb84b5f35a80f2
James Dabgotra <james@penguin.ws>
2012-10-06 15:58:48 -0700

    integration

--- public/assistant/assistant.js
+++ public/assistant/assistant.js
@@ -3,9 +3,19 @@ steal(
 	'./models/models.js',		// steals all your models
 	'./fixtures/fixtures.js',	// sets up fixtures for your models
 	'assistant/product/create',
-	'assistant/product/list',
-	function(){					// configure your application
+	'assistant/product/list')
+.then(function(){					// configure your application
 		
 		$('#products').assistant_product_list();
 		$('#create').assistant_product_create();
 })
+.then('jquery/jquery.masonry.js')
+.then(function(){
+	var $container = $('#products');
+	$container.imagesLoaded(function(){
+		$container.masonry({
+			itemSelector: 'li',
+			columnWidth: 90
+		});
+	});
+});
