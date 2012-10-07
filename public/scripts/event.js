c9822b9475185786bd6f4ea66e5c94fb706067e7
Patrick Woo <agileurbanite@gmail.com>
2012-10-06 22:15:55 -0700

    settin styles

--- public/scripts/event.js
+++ public/scripts/event.js
@@ -53,7 +53,8 @@ $(document).ready(function(){
                 $('#mens-styles').hide();
                 // get products based on selected styles
                 $.get('api/products', function(res){
-                   $('#products').html(res.result.html).show().masonry('reload'); 
+                   $('#products').html(res.result.html).show().masonry('reload');
+                   $('#search-bar').show();
                 });
                 
             }
@@ -75,6 +76,7 @@ $(document).ready(function(){
                 // get products based on selected styles
                 $.get('api/products', function(res){
                     $('#products').html(res.result.html).show().masonry('reload');
+                    $('#search-bar').show();
                 });
             }
         });
@@ -100,5 +102,48 @@ $(document).ready(function(){
                });
            }
         });
+    });
+    
+    // methods for autocomplete
+    function split( val ) {
+            return val.split( /,\s*/ );
+    }
+    function extractLast( term ) {
+            return split( term ).pop();
+    }
+    
+    // add autocomplete to search
+    $('#query').bind('keydown', function(event){
+        if ( event.keyCode === $.ui.keyCode.TAB && $( this ).data( "autocomplete" ).menu.active ) {
+            event.preventDefault();
+        }
+    }).autocomplete({
+            source: function( request, response ) {
+                    $.getJSON( "api/get-brands", {
+                            term: extractLast( request.term )
+                    }, response );
+            },
+            search: function() {
+                    // custom minLength
+                    var term = extractLast( this.value );
+                    if ( term.length < 2 ) {
+                            return false;
+                    }
+            },
+            focus: function() {
+                    // prevent value inserted on focus
+                    return false;
+            },
+            select: function( event, ui ) {
+                    var terms = split( this.value );
+                    // remove the current input
+                    terms.pop();
+                    // add the selected item
+                    terms.push( ui.item.value );
+                    // add placeholder to get the comma-and-space at the end
+                    terms.push( "" );
+                    this.value = terms.join( ", " );
+                    return false;
+            }
     });
 });
