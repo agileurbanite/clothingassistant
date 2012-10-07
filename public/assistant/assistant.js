909d7af29841a818055f99d4974c25f065872a74
James Dabgotra <james@penguin.ws>
2012-10-06 18:09:31 -0700

    blah

--- public/assistant/assistant.js
+++ public/assistant/assistant.js
@@ -3,11 +3,10 @@ steal(
 	'./models/models.js',		// steals all your models
 	'./fixtures/fixtures.js',	// sets up fixtures for your models
 	'assistant/product/create',
-	'assistant/product/list')
-.then(function(){					// configure your application
-		
-		$('#products').assistant_product_list();
-		$('#create').assistant_product_create();
+	'assistant/product/list') 
+.then(function(){					// configure your application	
+	$('#products').assistant_product_list();
+	$('#create').assistant_product_create();
 })
 .then('jquery/jquery.masonry.js')
 .then(function(){
